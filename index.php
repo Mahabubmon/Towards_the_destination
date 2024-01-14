3d4afca24b271cfddfa92b3aefe7db34d43ad8c8
Mahabubur Rahman <mahabubmon@gmail.com>
2024-01-14 12:54:49 +0600

    renew_the page

--- index.php
+++ index.php
@@ -5,14 +5,37 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/bootstrap.min.css" />
-  <link rel="stylesheet" href="./css/bootstrap.css" />
   <link rel="stylesheet" href="./css/all.min.css" />
   <link rel="stylesheet" href="./css/style.css" />
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <title>গন্তব্যের দিকে</title>
 </head>
 
 <body>
 
+  <!-- start nav-bar -->
+  <nav class="navbar navbar-expend-sm navbar-dark bg-danger pl-5 fixed-top">
+    <a href="index.php" class="navbar-brand">গন্তব্যের দিকে</a>
+    <span classs="nav-text">Give the best route for the customer's</span>
+
+    <button type="button" class="nav-bar-togler" data-toggle="collapse" data-target="#myMenue">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="myMenu">
+      <ul class="navbar-nav pl-5">
+        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+      </ul>
+    </div>
+
+  </nav>
+  <!-- End nav-bar -->
 
 
   <!-- javascript section -->
