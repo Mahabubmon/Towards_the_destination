48d7469e4b46996a16e3dd06dc4a4f6229dc351a
Mahabubur Rahman <mahabubmon@gmail.com>
2024-01-21 23:12:28 +0600

    try_new_way_1

--- new_index.php
+++ new_index.php
@@ -14,7 +14,8 @@
 
 <body>
 
-    <header class="sidebar">
+    <div class="sidebar">
+        <header>Test Case</header>
         <ul class="sidebarlink">
             <li><a href="#"><i class="fas fa-qrcode"></i>Home</a></li>
             <li><a href="#"><i class="fas fa-qrcode"></i>Content</a></li>
@@ -22,8 +23,15 @@
             <li><a href="#"><i class="fas fa-qrcode"></i>Content</a></li>
 
         </ul>
-    </header>
+    </div>
 
+
+
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+    <script src="./js/all.min.js"></script>
+    <script src="./js/script.js"></script>
 </body>
 
 </html>
