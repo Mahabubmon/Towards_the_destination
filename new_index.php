b1ee6f9733ff83ffd728d283438151efddf5ea49
Mahabubur Rahman <mahabubmon@gmail.com>
2024-01-22 22:31:19 +0600

    side_bar_jquery

--- new_index.php
+++ new_index.php
@@ -31,13 +31,22 @@
         </ul>
     </div>
 
+    <script>
+        $(document).ready(function () {
+            $('#btn').click(function () {
+                $('.sidebar').toggleClass('active');
+            });
+        });
 
+    </script>
 
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
     <script src="./js/all.min.js"></script>
     <script src="./js/script.js"></script>
+
+
 </body>
 
 </html>
