0069a76ca998b608fe623514ce1ac7e488d46b10
Mahabubur Rahman <mahabubmon@gmail.com>
2024-01-26 23:48:35 +0600

    reg_page_with_database connection and user create

--- registration.php
+++ registration.php
@@ -1,3 +1,18 @@
+<?php
+include 'db.php';
+
+
+if (isset($_POST["submit"])) {
+    $email = $_POST["email"];
+    $pass = $_POST["pass"];
+    $sql = mysqli_query($conn, "INSERT INTO `login_tbl`(`email`, `password`) VALUES ('$email','$pass')");
+
+} else {
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -47,27 +62,26 @@
 
             <h2 class="text-center mb-4">Registration</h2>
 
-            <form>
+            <form method="post">
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                    <label for="">Email address</label>
+                    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp"
                         placeholder="Enter email">
                     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
                         else.</small>
                 </div>
                 <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <label for="">Password</label>
+                    <input type="password" class="form-control" id="pass" name="pass" placeholder="Password">
                 </div>
                 <div class="form-group form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1">
                     <label class="form-check-label" for="exampleCheck1">I agree to the <a href="#">Terms of Service</a>
                         and <a href="#">Privacy Policy</a></label>
                 </div>
-                <button type="submit" class="btn btn-primary btn-block">Register</button>
-
+                <button type="submit" name="submit" class="btn btn-primary btn-block">Register</button>
                 <div class="signin">
-                    <p>Already have an account? <a href="#">Sign in</a>.</p>
+                    <p>Already have an account? <a href="login.php">Log in</a>.</p>
                 </div>
             </form>
 
