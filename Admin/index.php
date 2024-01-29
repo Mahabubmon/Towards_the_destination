96bc4cda3377b7c81f7696470d8bbf460a72005b
Mahabubur Rahman <mahabubmon@gmail.com>
2024-01-29 21:38:00 +0600

    login error

--- Admin/index.php
+++ Admin/index.php
@@ -1,12 +1,21 @@
+<?php
+session_start();
+
+include '../db.php';
+
+$username = isset($_SESSION['email']) ? $_SESSION['email'] : '';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./css/bootstrap.min.css" />
-    <link rel="stylesheet" href="./css/all.min.css" />
-    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="../css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../css/all.min.css" />
+    <link rel="stylesheet" href="../css/style.css" />
     <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -54,8 +63,8 @@
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
-    <script src="./js/all.min.js"></script>
-    <script src="./js/script.js"></script>
+    <script src="../js/all.min.js"></script>
+    <script src="../js/script.js"></script>
 
 
 
